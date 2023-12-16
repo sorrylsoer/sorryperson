94da8a4f07c96c57b62d5caf47efe755596344c8
sorrylsoer <154003322+sorrylsoer@users.noreply.github.com>
2023-12-16 06:52:09 -0800

    Add files via upload

---
+++ pokemon site/script.js
@@ -0,0 +1,203 @@
+const itemsList = [
+    { name: "Cull", image: "cull.jpg" },
+    { name: "Doran's Blade", image: "3134.png" },
+    { name: "Doran's Ring", image: "3140.png" },
+    { name: "Doran's Shield", image: "3105.png" },
+    { name: "Control Ward", image: "3340.png" },
+    { name: "Corrupting Potion", image: "3222.png" },
+    { name: "Amplifying Tome", image: "3916.png" },
+    { name: "B.F. Sword", image: "3123.png" },
+    { name: "Blasting Wand", image: "3135.png" },
+    { name: "Boots", image: "3158.png" },
+    { name: "Cloak of Agility", image: "6662.png" },
+    { name: "Cloth Armour", image: "4401.png" },
+    { name: "Dagger", image: "3600.png" },
+    { name: "Dark Seal", image: "6035.png" },
+    { name: "Long Sword", image: "4629.png" },
+    { name: "Needlessly Large Rod", image: "6029.png" },
+    { name: "Null-Magic Mantle", image: "3814.png" },
+    { name: "Maw of Malmortius", image: "3091.png" },
+    { name: "Manamune", image: "3508.png" },
+    { name: "Mortal Reminder", image: "3179.png" },
+    { name: "Morellonomicon", image: "3165.png" },
+    { name: "Mikael's Crucible", image: "3222.png" },
+    { name: "Mercury's Treads", image: "3111.png" },
+    { name: "Mercurial Scimitar", image: "3139.png" },
+    { name: "Mejai's Soulstealer", image: "3142.png" },
+    { name: "Warmog's Armor", image: "3083.png" },
+    { name: "Void Staff", image: "3135.png" },
+    { name: "Umbral Glaive", image: "3179.png" },
+    { name: "Twin Shadows", image: "3152.png" },
+    { name: "Trinity Force", image: "3100.png" },
+    { name: "Titanic Hydra", image: "3085.png" },
+    { name: "Thornmail", image: "3082.png" },
+    { name: "Sunfire Cape", image: "3121.png" },
+    { name: "Stormrazor", image: "6670.png" },
+    { name: "Sterak's Gage", image: "6672.png" },
+    { name: "Edge of Night", image: "6609.png" },
+    { name: "Frozen Heart", image: "6616.png" },
+    { name: "Frozen Mallet", image: "6655.png" },
+    { name: "Gargoyle Stoneplate", image: "3143.png" },
+    { name: "Guardian Angel", image: "4403.png" },
+    { name: "Guinsoo's Rageblade", image: "3124.png" },
+    { name: "Hextech GLP-800", image: "3145.png" },
+    { name: "Hextech Gunblade", image: "3094.png" },
+    { name: "Hextech Protobelt-01", image: "3152.png" },
+    { name: "Infinity Edge", image: "6675.png" },
+    { name: "Ionian Boots of Lucidity", image: "3317.png" },
+    { name: "Knight's Vow", image: "3109.png" },
+    { name: "Liandry's Torment", image: "6653.png" },
+    { name: "Lich Bane", image: "3100.png" },
+    { name: "Locket of the Iron Solari", image: "3357.png" },
+    { name: "Lord Dominik's Regards", image: "6631.png" },
+    { name: "Luden's Echo", image: "6655.png" },
+    { name: "Zhonya's Hourglass", image: "3157.png" },
+    { name: "Zeke's", image: "3504.png" }
+];
+    // Add more items with or without their respective image URLs
+  ;
+  const championsList = [
+    { name: 'Aatrox', image: 'Aatrox_0.jpg' },
+    { name: 'Ahri', image: 'Ahri_0.jpg' },
+    { name: 'Akali', image: 'Akali_0.jpg' },
+    { name: 'Alistar', image: 'alistar.jpg' },
+    { name: 'Amumu', image: 'amumu.jpg' },
+    { name: 'Anivia', image: 'anivia.jpg' },
+    { name: 'Annie', image: 'annie.jpg' },
+    { name: 'Ashe', image: 'ashe.jpg' },
+    { name: 'Aurelion Sol', image: 'aurelionsol.jpg' },
+    { name: 'Azir', image: 'azir.jpg' },
+    { name: 'Bard', image: 'bard.jpg' },
+    { name: 'Blitzcrank', image: 'blitzcrank.jpg' },
+    { name: 'Brand', image: 'brand.jpg' },
+    { name: 'Braum', image: 'braum.jpg' },
+    { name: 'Caitlyn', image: 'caitlyn.jpg' },
+    { name: 'Camille', image: 'camille.jpg' },
+    { name: 'Cassiopeia', image: 'cassiopeia.jpg' },
+    { name: 'Cho\'Gath', image: 'chogath.jpg' },
+    { name: 'Corki', image: 'corki.jpg' },
+    { name: 'Darius', image: 'darius.jpg' },
+    { name: 'Diana', image: 'diana.jpg' },
+    { name: 'Dr. Mundo', image: 'drmundo.jpg' },
+    { name: 'Draven', image: 'draven.jpg' },
+    { name: 'Ekko', image: 'ekko.jpg' },
+    { name: 'Elise', image: 'elise.jpg' },
+    { name: 'Evelynn', image: 'evelynn.jpg' },
+    { name: 'Ezreal', image: 'ezreal.jpg' },
+    { name: 'Fiddlesticks', image: 'fiddlesticks.jpg' },
+    { name: 'Fiora', image: 'fiora.jpg' },
+    { name: 'Fizz', image: 'fizz.jpg' },
+    { name: 'Galio', image: 'galio.jpg' },
+    { name: 'Gangplank', image: 'gangplank.jpg' },
+    { name: 'Garen', image: 'garen.jpg' },
+    { name: 'Gnar', image: 'gnar.jpg' },
+    { name: 'Gragas', image: 'gragas.jpg' },
+    { name: 'Graves', image: 'graves.jpg' },
+    { name: 'Hecarim', image: 'hecarim.jpg' },
+    { name: 'Heimerdinger', image: 'heimerdinger.jpg' },
+    { name: 'Illaoi', image: 'illaoi.jpg' },
+    { name: 'Irelia', image: 'irelia.jpg' },
+    { name: 'Ivern', image: 'ivern.jpg' },
+    { name: 'Janna', image: 'janna.jpg' },
+    { name: 'Jarvan IV', image: 'jarvaniv.jpg' },
+    { name: 'Jax', image: 'jax.jpg' },
+    { name: 'Jayce', image: 'jayce.jpg' },
+    { name: 'Jhin', image: 'jhin.jpg' },
+    { name: 'Jinx', image: 'jinx.jpg' },
+    { name: 'Kai\'Sa', image: 'kaisa.jpg' },
+    { name: 'Kalista', image: 'kalista.jpg' },
+    { name: 'Karma', image: 'karma.jpg' },
+    { name: 'Karthus', image: 'karthus.jpg' },
+    { name: 'Kassadin', image: 'kassadin.jpg' },
+    { name: 'Katarina', image: 'katarina.jpg' },
+    { name: 'Kayle', image: 'kayle.jpg' },
+    { name: 'Kayn', image: 'kayn.jpg' },
+    { name: 'Kennen', image: 'kennen.jpg' },
+    { name: 'Kha\'Zix', image: 'khazix.jpg' },
+    { name: 'Kindred', image: 'kindred.jpg' },
+    { name: 'Kled', image: 'kled.jpg' },
+    { name: 'Kog\'Maw', image: 'kogmaw.jpg' },
+    { name: 'LeBlanc', image: 'leblanc.jpg' },
+    { name: 'Lee Sin', image: 'leesin.jpg' },
+    { name: 'Leona', image: 'leona.jpg' },
+    { name: 'Lissandra', image: 'lissandra.jpg' },
+    { name: 'Lucian', image: 'lucian.jpg' },
+    { name: 'Lulu', image: 'lulu.jpg' },
+    { name: 'Lux', image: 'lux.jpg' },
+    { name: 'Malphite', image: 'malphite.jpg' },
+    { name: 'Malzahar', image: 'malzahar.jpg' },
+    { name: 'Maokai', image: 'maokai.jpg' },
+    { name: 'Master Yi', image: 'masteryi.jpg' },
+    { name: 'Miss Fortune', image: 'missfortune.jpg' },
+    { name: 'Mordekaiser', image: 'mordekaiser.jpg' },
+    { name: 'Morgana', image: 'morgana.jpg' },
+    { name: 'Nami', image: 'nami.jpg' },
+    { name: 'Nasus', image: 'nasus.jpg' },
+    { name: 'Nautilus', image: 'nautilus.jpg' },
+    { name: 'Neeko', image: 'neeko.jpg' },
+    { name: 'Nidalee', image: 'nidalee.jpg' },
+    { name: 'Nocturne', image: 'nocturne.jpg' },
+    { name: 'Nunu & Willump', image: 'nunu.jpg' },
+    { name: 'Olaf', image: 'olaf.jpg' },
+    { name: 'Orianna', image: 'orianna.jpg' },
+    { name: 'Ornn', image: 'ornn.jpg' },
+    { name: 'Pantheon', image: 'pantheon.jpg' },
+
+  
+  ];
+function modifyImageFileName(imageFileName) {
+    const championName = imageFileName.split('.')[0]; // Extract the champion name without extension
+    const formattedChampionName = championName.charAt(0).toUpperCase() + championName.slice(1); // Capitalize the first letter
+    return `${formattedChampionName}_0.jpg`;
+  }
+  
+  // Modify all champion image filenames in the championsList
+  for (const champion of championsList) {
+    champion.image = modifyImageFileName(champion.image);
+  }
+  
+  // Output the modified champion list for verification
+  console.log(championsList);
+  
+  function generateRandomBuild() {
+    alert("Good Luck!");
+  
+    const buildDisplay = document.getElementById('buildDisplay');
+    buildDisplay.innerHTML = ''; // Clear previous content
+  
+    const randomChampion = championsList[Math.floor(Math.random() * championsList.length)];
+    const championElement = document.createElement('div');
+    championElement.classList.add('champion');
+    championElement.innerHTML = `
+      <h2>Champion:</h2>
+      <p>${randomChampion.name}</p>
+      <img src="${randomChampion.image}" alt="${randomChampion.name} Image" />
+    `;
+    buildDisplay.appendChild(championElement);
+  
+    for (let i = 0; i < 3; i++) {
+      const randomItem = itemsList[Math.floor(Math.random() * itemsList.length)];
+  
+      const itemElement = document.createElement('div');
+      itemElement.classList.add('item');
+  
+      if (randomItem.image) {
+        const itemContent = `
+          <h2>Item ${i + 1}:</h2>
+          <p>${randomItem.name}</p>
+          <img src="${randomItem.image}" alt="${randomItem.name} Image" />
+        `;
+        itemElement.innerHTML = itemContent;
+      } else {
+        const itemContent = `
+          <h2>Item ${i + 1}:</h2>
+          <p>${randomItem.name}</p>
+          <p>No image available for ${randomItem.name}</p>
+        `;
+        itemElement.innerHTML = itemContent;
+      }
+  
+      buildDisplay.appendChild(itemElement);
+    }
+    
+  }
